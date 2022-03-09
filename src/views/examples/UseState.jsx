f4fa59f2dc3b97d03b0ed4063c0c29292cb6a71e
Eder Gracional <Emailedergacional@hotmail.com>
2022-03-09 11:59:57 -0300

    62. usestate #02

--- src/views/examples/UseState.jsx
+++ src/views/examples/UseState.jsx
@@ -4,6 +4,8 @@ import SectionTitle from './../../components/layout/SectionTitle';
 
 const UseState = (props) => {
     const [count, setCount] = useState(0)
+    const [name, setName] = useState("")
+
 
 
     return (
@@ -14,8 +16,10 @@ const UseState = (props) => {
 
         <SectionTitle title="Exercício #01"/>
             <div className="center">
+                {/*usamos span mostrar o valor */}
                 <span className="text">{count}</span>
                 <div>
+                    {/*botões para alterar o valor */}
                     <button className="btn"
                         onClick={() => setCount(count -1)}>-1</button>
                     <button className="btn"
@@ -26,9 +30,24 @@ const UseState = (props) => {
                 </div>
             
             </div>
-        <SectionTitle title="Exercício #02"/>           
+        <SectionTitle title="Exercício #02"/>
+        {/*usamos input para controlar a evolução do input*/}
+        <input type="text" className="input" 
+            value={name} oneChange={e => setName(e.target.value)} />
+            <span className='text'>{name}</span>          
         </div>
     )
 }
 
 export default UseState
+
+
+/* 
+    Foram feitos 2 exemlos com  useStates: 
+
+        Controlando um valor numérico 
+        const [count, setCount] = useState(0)
+
+        Controlando um valor textual
+        const [name, setName] = useState("")
+*/
